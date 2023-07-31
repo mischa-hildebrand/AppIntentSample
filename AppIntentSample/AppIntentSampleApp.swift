783e1a02fa418a2aaa8bb1d93575e6f0a17a3f34
Mischa Hildebrand <web@mischa-hildebrand.de>
2023-07-31 23:12:45 +0200

    Initial Commit

---
+++ AppIntentSample/AppIntentSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentSampleApp.swift
+//  AppIntentSample
+//
+//  Created by Mischa on 31.07.23.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
